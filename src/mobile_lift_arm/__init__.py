62f5b55054d1917790f0cfab990981b9ad437fbe
pvd0anh <pvdoanh.ce@gmail.com>
2025-10-08 23:25:22 +0700

    new

--- src/mobile_lift_arm/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Top level package for the scissor‑lift control example.
-
-This package contains a basic controller and GUI for a simple
-scissor‑lift model.  The code has been refactored from the original
-repository into a proper Python package so it can be installed with
-setuptools and used as console entry points.  See the individual
-modules for more details.
-"""
